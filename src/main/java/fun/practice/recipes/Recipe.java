20155a322a707f4c979335676cf3f9a694d5c000
AndyDeveloper <86152791+JohnyOhJohny@users.noreply.github.com>
2022-09-03 13:35:09 +0300

    Update

--- src/main/java/fun/practice/recipes/Recipe.java
+++ src/main/java/fun/practice/recipes/Recipe.java
@@ -1,23 +1,33 @@
 package fun.practice.recipes;
 
+import java.util.Arrays;
+
 public class Recipe {
 
     private String name;
     private String description;
-    private String ingredients;
-    private String directions;
+    private String[] ingredients;
+    private String[] directions;
 
     // Constructors
-    public Recipe(String name, String description, String ingredients, String directions) {
+    public Recipe(String name, String description, String[] ingredients, String[] directions) { // Previously was also int id
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.directions = directions;
     }
 
-    public Recipe(){}
+    public Recipe() {
+    }
 
     // Setters
+
+    /*
+    public void setId(int id) {
+        this.id = id;
+    }
+
+     */
 
     public void setName(String name) {
         this.name = name;
@@ -27,15 +37,22 @@ public class Recipe {
         this.description = description;
     }
 
-    public void setIngredients(String ingredients) {
+    public void setIngredients(String[] ingredients) {
         this.ingredients = ingredients;
     }
 
-    public void setDirections(String directions) {
+    public void setDirections(String[] directions) {
         this.directions = directions;
     }
 
     // Getters
+
+    /*
+    public int getId() {
+        return this.id = id;
+    }
+
+     */
 
     public String getName() {
         return name;
@@ -45,12 +62,22 @@ public class Recipe {
         return description;
     }
 
-    public String getIngredients() {
+    public String[] getIngredients() {
         return ingredients;
     }
 
-    public String getDirections() {
+    public String[] getDirections() {
         return directions;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", ingredients=" + Arrays.toString(ingredients) +
+                ", directions=" + Arrays.toString(directions) +
+                '}';
     }
 
 }
--- src/main/java/fun/practice/recipes/RecipesApplication.java
+++ src/main/java/fun/practice/recipes/RecipesApplication.java
@@ -1,11 +1,14 @@
 package fun.practice.recipes;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus; // This
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException; // This
 
 @SpringBootApplication
 public class RecipesApplication {
@@ -18,18 +21,44 @@ public class RecipesApplication {
 	@RestController
 	public class RecipeController {
 
-		Recipe singleRecipe = new Recipe();
+		// Create collection of recipes, where You will add all Your recipes
+		Map<Integer, Recipe> collectionOfRecipes = new HashMap<>();
+		int idNumbering = 1;
 
-		@PostMapping("/api/recipe")
-		public Recipe postRecipe(@RequestBody Recipe recipe) {
-			singleRecipe = recipe;
-			System.out.println("Your recipe has been successfully added!");
-			return singleRecipe;
+		// Endpoint for importing new recipes
+		@PostMapping("/api/recipe/new")
+		public Id postRecipe(@RequestBody Recipe recipe) { // Previous type: String
+			Id newId = new Id(idNumbering);
+			collectionOfRecipes.put(newId.getId(), recipe);
+
+            /*
+            // Temp
+            System.out.println("Your recipe has been successfully added!");
+
+            // Temp: Print the whole recipe book
+            for (Map.Entry<Integer, Recipe> entry : collectionOfRecipes.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue().toString());
+            }
+             */
+
+			System.out.println(newId); // Temp
+			idNumbering++;
+			return newId;
 		}
 
-		@GetMapping("/api/recipe")
-		public Recipe getRecipe() {
-			return singleRecipe;
+
+
+		// Endpoint for displaying existing recipes- according to the requested id
+		@GetMapping("/api/recipe/{id}")
+		public Recipe getRecipe(@PathVariable int id) {
+			if (!collectionOfRecipes.containsKey(id)) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+						"Unfortunately recipe with such id is not found");
+			} else {
+				System.out.println(collectionOfRecipes.get(id)); // Temp
+				return collectionOfRecipes.get(id);
+			}
+
 		}
 
 	}
